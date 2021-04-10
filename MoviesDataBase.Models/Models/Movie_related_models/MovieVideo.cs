51f7e121dd49728178a805f537cfb5c39c6cadfb
Ibrahim jaber <jber595@gmail.com>
2021-04-10 09:18:13 -0300

    Adding missing references
    Adding missing references

---
+++ MoviesDataBase.Models/Models/Movie_related_models/MovieVideo.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesDataBase.Models.Models.Movie_related_models
+{
+    public class MovieVideo
+    {
+        [JsonProperty("id")]
+        public string ID { get; set; }
+
+        [JsonProperty("key")]
+        public string Key { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("site")]
+        public string Site { get; set; }
+
+        [JsonProperty("size")]
+        public string Size { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+}
---
+++ MoviesDataBase.Models/Models/Movie_related_models/SimpleMovie.cs
@@ -0,0 +1,16 @@
+﻿using MoviesDataBase.Models.Models.Movie_related_models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesDataBase.Models.Models
+{
+    public class SimpleMovie : Movie
+    {
+        [JsonProperty("genre_ids")]
+        public List<string> Genres { get; set; }
+    }
+}
